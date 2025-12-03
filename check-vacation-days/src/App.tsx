6f6c38f6b07caaffd9eca2684ee3d2b150dada9c
Sergey Baranov <workinggrid@gmail.com>
2025-12-03 22:36:59 +0300

    updated css

--- check-vacation-days/src/App.tsx
+++ check-vacation-days/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Flex, Layout } from 'antd';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const contentStyle: React.CSSProperties = {
+  textAlign: 'center',
+  minWidth: '100vw',
+  minHeight: '100vh',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#0958d9',
+};
 
-export default App
+
+const App: React.FC = () => (
+  <Flex gap="middle">
+    <Layout>
+      <Layout.Content style={contentStyle}>Content</Layout.Content>
+    </Layout>
+  </Flex>
+  );
+
+export default App;
