9bb2c0fafe2dfa30f5a362800321fd667914c935
Sergey Baranov <workinggrid@gmail.com>
2025-12-07 13:51:42 +0300

    added functionality to send the request

--- check-vacation-days/src/components/show-vacations-dates/ShowVacationdatesCard.tsx
+++ check-vacation-days/src/components/show-vacations-dates/ShowVacationdatesCard.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
-import { Layout, Card, Typography } from 'antd';
+import { Layout, Card, Typography, Alert, Button, notification  } from 'antd';
+import dayjs from 'dayjs';
 
-const { Paragraph, Title } = Typography;
+const { Paragraph, Title, Text} = Typography;
 
 interface ShowVacationdatesCardProps {
   ukgDays: number;
   rfDays: number;
+  plannedVacationDays?: string[]; //показываем какие дни пользователь забронировал
 }
 
-export const ShowVacationdatesCard: React.FC<ShowVacationdatesCardProps> = ({ukgDays, rfDays}) => (
-  <>
-    <Layout style={{ maxWidth: '300px', background: 'transparent', textAlign: 'left' }}>
+export const ShowVacationdatesCard: React.FC<ShowVacationdatesCardProps> = ({ukgDays, rfDays, plannedVacationDays = [],}) => {
+  {/**проверяем что количество дней меньше или равно дням по ТК РФ и если условие верно - показываем или показываем с ошибкой*/}
+  const plannedDaysCount = plannedVacationDays.length;
+  const isDaysExceeded = (plannedDaysCount) > rfDays;
+
+  // получаем диапазон дат
+  const startDate = plannedVacationDays[0] ? dayjs(plannedVacationDays[0]).format('DD.MM.YYYY') : null;
+  const endDate = plannedVacationDays[1] ? dayjs(plannedVacationDays[1]).format('DD.MM.YYYY') : null;
+
+  const handleSend = () => {
+    notification.success({
+      message: 'Запрос отправлен',
+      description: 'Ваш запрос на отпуск отправлен в отдел кадров.',
+      placement: 'topRight',
+      title: undefined
+    });
+  };
+
+  return (
+  
+    <Layout style={{ maxWidth: '300px', background: 'transparent', textAlign: 'left', gap: '1rem' }}>
+      
       <Card>
         <Paragraph>
           Количество свободных дней по UKG
@@ -25,7 +46,44 @@ export const ShowVacationdatesCard: React.FC<ShowVacationdatesCardProps> = ({ukg
           {rfDays || 0}
         </Title>
       </Card>
+      <Card>
+        <Paragraph>
+          <Text type="secondary">
+            Запланированное количество дней отпуска
+          </Text>
+          <Title
+            level={1}
+            style={{
+              margin: '0',
+              color: isDaysExceeded ? 'red' : undefined}}>
+            {plannedVacationDays.length}
+          </Title>
+          {/* выводим диапазон дней */}
+          {startDate && endDate && (
+            <Paragraph style={{ fontSize: '0.85rem', marginTop: '0.5rem' }}>
+              Запланированный отпуск с {startDate} по {endDate}
+            </Paragraph>
+          )}
+        </Paragraph>
+        {/* проверяем что количество дней меньше или равно дням по ТК РФ */}
+        {isDaysExceeded && (
+          <Alert
+            description="Количество запланированных дней превышает допустимое количество дней по ТК РФ. Выберите другие даты или обновите количество дней отпуска."
+            type="error"
+            showIcon
+          />
+        )}
+        <Button
+          type="primary"
+          onClick={handleSend}
+          disabled={isDaysExceeded || plannedDaysCount === 0}
+          style={{ width: '100%' }}
+        >
+          Отправить запрос
+        </Button>
+        
+      </Card>
     </Layout>
-  </>
-)
+  )
+};
 
--- check-vacation-days/src/components/top-title/TopTitle.tsx
+++ check-vacation-days/src/components/top-title/TopTitle.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Typography  } from 'antd';
 
-const { Title } = Typography;
+const { Title, Paragraph, Text } = Typography;
 
 export const TopTitle: React.FC = () => (
   <>
-    <Title level={2} style={{color: '#fff', marginBottom: '2rem', textAlign: 'left'}}>Дни отпуска</Title>
+    <Title level={2} style={{color: '#fff', textAlign: 'left'}}>Дни отпуска</Title>
+    <Paragraph style={{textAlign: 'left',  marginBottom: '2rem'}}>
+      <Text type="secondary" style={{color: '#fff'}}>
+        Обратите внимание, что количество дней отпуска по ТК РФ может отличаться от количества дней, предоставляемых системой UKG. Поскольку система UKG не учитывает выходные дни и праздничные дни при расчете отпускных дней, в то время как Трудовой кодекс Российской Федерации предусматривает их включение в общий отпускной период.
+      </Text>
+    </Paragraph>
   </>
 )
