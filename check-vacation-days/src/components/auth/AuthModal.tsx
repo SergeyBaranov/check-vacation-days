1ebe66679b9798e73618ba885b82b8eb11e84d7b
Sergey Baranov <workinggrid@gmail.com>
2026-01-09 23:07:59 +0300

    added login form gor the user

---
+++ check-vacation-days/src/components/auth/AuthModal.tsx
@@ -0,0 +1,77 @@
+import { Modal, Input, Tabs, message, Button } from "antd";
+import { useState } from "react";
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+  onLogin: (username: string) => void;
+};
+
+export const AuthModal: React.FC<Props> = ({ open, onClose, onLogin }) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async () => {
+    const res = await fetch("http://localhost:3000/api/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, email, password })
+    });
+    const data = await res.json();
+    if (data.error) {
+      message.error(data.error);
+    } else {
+      message.success(data.message);
+    }
+  };
+
+  const login = async () => {
+    const res = await fetch("http://localhost:3000/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password })
+    });
+    const data = await res.json();
+
+    if (data.error) {
+      message.error(data.error);
+    } else {
+      message.success("Вы вошли");
+      onLogin(username || email);
+      onClose();
+    }
+  };
+
+  return (
+    <Modal open={open} onCancel={onClose} footer={null} title="Авторизация">
+      <Tabs
+        items={[
+          {
+            key: "login",
+            label: "Вход",
+            children: (
+              <>
+                <Input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+                <Input.Password placeholder="Пароль" onChange={e => setPassword(e.target.value)} style={{ marginTop: 8 }} />
+                <button onClick={login} style={{ marginTop: 12 }}>Войти</button>
+              </>
+            )
+          },
+          {
+            key: "register",
+            label: "Регистрация",
+            children: (
+              <>
+                <Input placeholder="Username" onChange={e => setUsername(e.target.value)} />
+                <Input placeholder="Email" onChange={e => setEmail(e.target.value)} style={{ marginTop: 8 }} />
+                <Input.Password placeholder="Пароль" onChange={e => setPassword(e.target.value)} style={{ marginTop: 8 }} />
+                <Button type="primary" onClick={register} style={{ marginTop: 12 }}>Зарегистрироваться</Button>
+              </>
+            )
+          }
+        ]}
+      />
+    </Modal>
+  );
+};
--- check-vacation-days/src/components/top-title/TopTitle.tsx
+++ check-vacation-days/src/components/top-title/TopTitle.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Typography  } from 'antd';
+import { Layout, Typography  } from 'antd';
+import { UserAvatar } from '../user/userAvatar';
+import { useState } from 'react';
 
 const { Title, Paragraph, Text } = Typography;
 
-export const TopTitle: React.FC = () => (
-  <>
-    <Title level={2} style={{color: '#fff', textAlign: 'left'}}>Дни отпуска</Title>
+
+export const TopTitle: React.FC = () => {
+  const [user, setUser] = useState<string | null>(null);
+
+  return <>
+    <Layout style={{background: 'transparent', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'baseline', marginBottom: '1rem' }}>
+      <Title level={2} style={{color: '#fff', textAlign: 'left', width: '100%'}}>Дни отпуска</Title>
+      <UserAvatar onLogin={setUser} user={user ?? undefined} />
+    </Layout>
     <Paragraph style={{textAlign: 'left',  marginBottom: '2rem'}}>
       <Text type="secondary" style={{color: '#fff'}}>
         Обратите внимание, что количество дней отпуска по ТК РФ может отличаться от количества дней, предоставляемых системой UKG. Поскольку система UKG не учитывает выходные дни и праздничные дни при расчете отпускных дней, в то время как Трудовой кодекс Российской Федерации предусматривает их включение в общий отпускной период.
       </Text>
     </Paragraph>
   </>
-)
+}
